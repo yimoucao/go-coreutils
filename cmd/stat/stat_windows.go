fa22675bddffdf09df5eadd5fa0934bd778ba758
yimoucao <yimoucao@gmail.com>
2020-12-25 18:17:23 -0800

    stat: support windows

--- cmd/stat/stat_windows.go
+++ cmd/stat/stat_windows.go
@@ -1,8 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/user"
+	"syscall"
+	"time"
+)
 
 func stat(filename string) error {
-	fmt.Println("not implemented")
+	stat, err := os.Stat(filename)
+	if err != nil {
+		return err
+	}
+	sys := stat.Sys().(*syscall.Win32FileAttributeData)
+	fmt.Printf("%+v\n", sys)
+
+	fmt.Printf("File: %v\n", stat.Name())
+
+	fmt.Printf("Size: %v\t", stat.Size())
+	// fmt.Printf("Blocks: %v\t", sys.Blocks)
+	// fmt.Printf("IO Block: %v\t", sys.Blksize)
+	fmt.Printf("%v\n", modeTypeString(stat.Mode()))
+
+	// fmt.Printf("Device: %xh/%dd\t", sys.Dev, sys.Dev)
+	// fmt.Printf("Inode: %d\t", sys.Ino)
+	// fmt.Printf("Links: %d\n", sys.Nlink)
+
+	fmt.Printf("Access: (%#o/%v)\n", stat.Mode(), stat.Mode())
+	u, _ := user.Current()
+	fmt.Printf("Uid: (%v/%v)\n", u.Uid, u.Username)
+	grp, err := user.LookupGroupId(u.Gid)
+	if err != nil {
+		fmt.Printf("Gid: (%v/%v)\n", u.Gid, "UNKNOWN")
+	} else {
+		fmt.Printf("Gid: (%v/%v)\n", grp.Gid, grp.Name)
+	}
+
+	fmt.Printf("Access: %v\n", time.Unix(0, sys.LastAccessTime.Nanoseconds()))
+	fmt.Printf("Modify: %v\n", stat.ModTime())
+	fmt.Printf("Change: %v\n", stat.ModTime()) // see os/types_windows.go:136 ModTime()
+	fmt.Printf("Birth: %v\n", time.Unix(0, sys.CreationTime.Nanoseconds()))
 	return nil
 }
